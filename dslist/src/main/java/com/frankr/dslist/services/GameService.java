7a2259e91ca335b66233c7afc0af76c0501cba3b
Frank R Oliveira <frankkol@hotmail.com>
2023-07-11 21:30:19 -0300

    GET Game by ID

--- dslist/src/main/java/com/frankr/dslist/services/GameService.java
+++ dslist/src/main/java/com/frankr/dslist/services/GameService.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.frankr.dslist.dto.GameDTO;
 import com.frankr.dslist.dto.GameMinDTO;
 import com.frankr.dslist.entities.Game;
 import com.frankr.dslist.repositories.GameRepository;
+
 
 @Service
 public class GameService {
@@ -15,6 +18,14 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		GameDTO dto = new GameDTO(result);
+		return dto;
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
